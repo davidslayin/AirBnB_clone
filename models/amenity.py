fdac5c225e27220903c5caf72403617997472859
davidslayin <dave191994@gmail.com>
2022-11-29 12:31:47 -0800

    class representing amenity

---
+++ models/amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Module amenity
+Contain class Amenity
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """class that represent Amenity
+    Attributes
+    ----------
+    name : str
+        The name of the amenity
+    """
+    name = ""
